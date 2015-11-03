ffda085726e62d1969bc36cbe41d409ac9fb449f
Ryan Hamblin <ryanhca@gmail.com>
2015-11-02 22:32:27 -0600

    should revisit reject with a passion

--- src/underbar.js
+++ src/underbar.js
@@ -95,7 +95,13 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    
+    var results = [];
+    _.filter(collection, function(val){
+      if(!test(val)){
+        results.push(val);
+      }
+    });
+    return results;
 
   };
 
