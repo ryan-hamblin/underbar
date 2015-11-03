f298f983c93a89a6296588506a77f53def402c86
Ryan Hamblin <ryanhca@gmail.com>
2015-11-02 22:38:12 -0600

    redid Map

--- src/underbar.js
+++ src/underbar.js
@@ -107,6 +107,7 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    
   };
 
 
@@ -115,6 +116,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(item){
+      results.push(iterator(item));
+    });
+    return results;
   };
 
   /*
